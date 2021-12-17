ab33ed03d6bed1e0d1764c11c80ac4b0666f21d0
Deividwb <bernardinodeivid@gmail.com>
2021-12-16 23:51:44 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,15 @@
+package academy.devdojo.springboot2.domain;
+
+import javax.swing.*;
+
+public class Anime {
+
+    public String name;
+
+    public Anime() {
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+}
